fc14b2bef6fe9823d3c7f2dcabd6ea16b08d33fc
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-06-11 18:40:40 -0600

    Refactor create_A_B_C_D_keys method and tests for key_spec.rb

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -17,16 +17,26 @@ RSpec.describe Key do
 
   describe 'methods' do
     it 'can generate a random enigma_key' do
-      mock_key = double("Random Key")
-      allow(mock_key).to receive(:enigma_key).and_return('random')
-
-      expect(mock_key.enigma_key).to eq('random')
-
+      # mock_key = double("Random Key")
+      # allow(mock_key).to receive(:enigma_key).and_return('random')
+      #
+      # expect(mock_key.enigma_key).to eq('random')
       key = Key.new('random')
-      key.generate_key('random')
 
       expect(key.enigma_key).to be_a(String)
       expect(key.enigma_key.length).to eq(5)
+    end
+
+    it "can create A key, B key, C key, and D key" do
+      key = Key.new('02715')
+
+      expect(key.create_A_B_C_D_keys).to be_an(Array)
+      expect(key.create_A_B_C_D_keys).to eq([02, 27, 71, 15])
+
+      key = Key.new('random')
+
+      expect(key.create_A_B_C_D_keys).to be_an(Array)
+      expect(key.create_A_B_C_D_keys.length).to eq(4)
     end
   end
 end

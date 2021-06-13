d16ed256e43a36c608f7279d157878899451396c
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-06-13 12:47:27 -0600

    Add calculate shift method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,27 @@
 class Enigma
+  attr_reader :character_set
+
+  def initialize
+    @character_set = ("a".."z").to_a << " "
+  end
+
+  def calculate_shift(key, date)
+    enigma_key = Key.new(key)
+    key_array = enigma_key.create_A_B_C_D_keys
+    enigma_offset = Offset.new(date)
+    offset_array = enigma_offset.create_A_B_C_D_offset
+    shift_array = []
+    shift_array << key_array << offset_array
+    shift_array.transpose.map do |arr|
+      arr.reduce(0) do |sum, num|
+        sum + num
+      end
+    end
+  end
+
+  def encrypt(message, key, date)
+    calculate_shift(key, date)
+
+  end
+
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -8,10 +8,19 @@ RSpec.describe Enigma do
     it 'exists' do
       expect(@enigma).to be_an(Enigma)
     end
+
+    it 'has readable attributes' do
+      expect(@enigma.character_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+    end
   end
 
   describe 'methods' do
-    xit 'can encrypt a message with a key and date' do
+    it 'can calculate the shift' do
+      expect(@enigma.calculate_shift("02715", "040895")).to be_an(Array)
+      expect(@enigma.calculate_shift("02715", "040895")).to eq([3, 27, 73, 20])
+    end
+
+    it 'can encrypt a message with a key and date' do
       expect(@enigma.encrypt("hello world", "02715", "040895")).to eq({
         encryption: "keder ohulw",
         key: "02715",

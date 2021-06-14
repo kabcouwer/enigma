77ccbba71a010ecf4031b3adbf997aa680f95058
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-06-14 10:39:19 -0600

    create find_rotation(index) method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,27 +1,50 @@
 class Enigma
-  attr_reader :character_set
+  attr_reader :character_set,
+              :shift_array
 
   def initialize
+    @shift_array = [],
     @character_set = ("a".."z").to_a << " "
   end
 
+
+  # def encrypt(message, key = 'random', date = 'today')
+  #   encryption = []
+  #   shift = calculate_shift(key, date)
+  #   message_array = message.chars
+  #   message_array.map do |letter|
+  #     i = character_set.find_index(letter)
+  #     new_letter = character_set[i + 1]
+  #     encryption << new_letter
+  #   end
+  # end
+
   def calculate_shift(key, date)
+    shift = []
     enigma_key = Key.new(key)
     key_array = enigma_key.create_A_B_C_D_keys
     enigma_offset = Offset.new(date)
     offset_array = enigma_offset.create_A_B_C_D_offset
-    shift_array = []
-    shift_array << key_array << offset_array
-    shift_array.transpose.map do |arr|
+    shift << key_array << offset_array
+    @shift_array = shift.transpose.map do |arr|
       arr.reduce(0) do |sum, num|
         sum + num
       end
+    end.flatten
+  end
+
+  def find_rotation(index)
+    if index == 0 || index % 4 == 0
+      @shift_array[0]
+    elsif index == 1 || index % 4 == 1
+      @shift_array[1]
+    elsif index == 2 || index % 4 == 2
+      @shift_array[2]
+    elsif index == 3 || index % 4 == 3
+      @shift_array[3]
     end
   end
 
-  def encrypt(message, key, date)
-    calculate_shift(key, date)
 
-  end
 
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -20,7 +20,24 @@ RSpec.describe Enigma do
       expect(@enigma.calculate_shift("02715", "040895")).to eq([3, 27, 73, 20])
     end
 
-    it 'can encrypt a message with a key and date' do
+    it 'can determine which shift is occuring depending on the index' do
+      @enigma.calculate_shift("02715", "040895")
+
+      expect(@enigma.find_rotation(0)).to eq(3)
+      expect(@enigma.find_rotation(1)).to eq(27)
+      expect(@enigma.find_rotation(2)).to eq(73)
+      expect(@enigma.find_rotation(3)).to eq(20)
+      expect(@enigma.find_rotation(4)).to eq(3)
+      expect(@enigma.find_rotation(5)).to eq(27)
+      expect(@enigma.find_rotation(6)).to eq(73)
+      expect(@enigma.find_rotation(7)).to eq(20)
+      expect(@enigma.find_rotation(8)).to eq(3)
+      expect(@enigma.find_rotation(9)).to eq(27)
+      expect(@enigma.find_rotation(10)).to eq(73)
+      expect(@enigma.find_rotation(11)).to eq(20)
+    end
+
+    xit 'can encrypt a message with a key and date' do
       expect(@enigma.encrypt("hello world", "02715", "040895")).to eq({
         encryption: "keder ohulw",
         key: "02715",
